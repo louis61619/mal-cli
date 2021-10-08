6c76e316b9996e0c95f6fb9903eac669bdeacf0e
louis <louis61619@gmail.com>
2021-10-08 17:22:48 +0800

    feat(root): prepare local cache and modify readme

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const program = require('commander')
 const { helpOptions } = require('./lib/core/help')
 const creareCommands = require('./lib/core/create')
 
+// 創建本地緩存
+
 // 查看版本號
 program.version(require('./package.json').version, '-v', '--version') // 新增-v的參數設置
 program.version(`v${require('./package.json').version}`).usage('<command> [option]')
--- readme.md
+++ readme.md
@@ -1,9 +1,17 @@
+you can use this cli tools to manage your template on github
+
 just enter
 
 ```
 npm i -g mal-cli
 ```
 
+then
+
 ```
 mal-cli create my-app
 ```
+
+you can see demo template
+
+if you want to change setting, you can modify setting file in ~/.mal-cli
